24df3f9f03c74c2f8909a16016fddd32e457b655
Ame-ReIori <reiori_ame@outlook.com>
2023-12-16 10:38:10 +0000

    [bugfix] PRG() encrypts different plaintext now.

--- src/dlen_prng.cc
+++ src/dlen_prng.cc
@@ -1,15 +1,16 @@
 #include "dlen_prng.h"
 
-const osuCrypto::block pt[2] = { osuCrypto::ZeroBlock, osuCrypto::OneBlock };
 
 void PRG(osuCrypto::block s, osuCrypto::block *rand) {
   osuCrypto::AES aes(s);
+  osuCrypto::block pt[2] = { osuCrypto::ZeroBlock, osuCrypto::OneBlock };
 
   aes.ecbEncBlocksInline<2>(pt, rand);
 }
 
 void HalfPRG(osuCrypto::block s, osuCrypto::block& rand, uint8_t b) {
   osuCrypto::AES aes(s);
+  osuCrypto::block pt[2] = { osuCrypto::ZeroBlock, osuCrypto::OneBlock };
 
   aes.ecbEncBlock(pt[b], rand);
 }
